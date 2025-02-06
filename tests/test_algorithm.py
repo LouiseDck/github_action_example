8bb90fa98b193c5e61edc604ba13c8edfcfba4da
LouiseDck <louise.deconinck@gmail.com>
2025-02-06 14:26:00 +0100

    Add test

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -5,6 +5,12 @@ def test_algorithm():
     result = very_complex_algorithm(5, 5)
     assert result.shape == (3, 7)
 
+def test_result():
+    # test that all elements are between 1 and 100
+    result = very_complex_algorithm(5, 5)
+    assert (result >= 1).all()
+    assert (result <= 100).all()
+
 def test_wrong_params():
     with pytest.raises(ValueError):
         very_complex_algorithm(0, 0)
